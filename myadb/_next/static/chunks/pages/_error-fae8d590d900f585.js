3e02b683b91f59534920dd37061bdfbe43c21e25
liou <liou@wondershare.cn>
2025-02-19 16:26:27 +0800

    scrcpy android 15 support

---
+++ myadb/_next/static/chunks/pages/_error-fae8d590d900f585.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{1108:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(7483)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=1108)}),_N_E=n.O()}]);
+//# sourceMappingURL=_error-fae8d590d900f585.js.map
---
+++ myadb/_next/static/chunks/pages/_offline-6d6f8a1e72491e3f.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95],{912:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_offline",function(){return e(7205)}])},7205:function(n,u,e){"use strict";e.r(u),e.d(u,{default:function(){return r}});var t=e(7531),f=e.n(t),c=e(4411);function r(){return(0,c.useEffect)(()=>{f().replace(location.href)},[]),null}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=912)}),_N_E=n.O()}]);
+//# sourceMappingURL=_offline-6d6f8a1e72491e3f.js.map
